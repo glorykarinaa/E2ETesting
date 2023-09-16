4f3eeadc0036e0636f9b3b64717c9c62e1ba41df
Glory Karina Hotbana Sitohang <60728807+glorykarinaa@users.noreply.github.com>
2023-09-16 15:47:59 +0700

    Update createaccbank.cy.js

--- Cypress-Test/cypress/e2e/1-getting-started/createaccbank.cy.js
+++ Cypress-Test/cypress/e2e/1-getting-started/createaccbank.cy.js
@@ -2,8 +2,8 @@ describe('Verify user can add a bank account', () => {
 
     it ('Verify fill in the bank account form with valid data', () => {
          cy.visit("https://pensiunq.staging.ittron.co.id/dplk/auth/login")
-         cy.get('#email').type('mandiridplk1@yopmail.com')
-         cy.get('#password').type('Ittronoke354!')
+         cy.get('#email').type('....')
+         cy.get('#password').type('.....')
          cy.get('.btn').click()
          cy.get('.welcome').contains('Selamat Datang')
          cy.get('.sidenav-inner > :nth-child(3) > .sidenav-link').click()
